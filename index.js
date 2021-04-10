7fcdd3eaead2c8b9b2db24fdb4d3d1e6e4d1bd9b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-10 13:36:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ function testFunc() {
   return "hi"
 }
 
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-  //...
-]
+superbowlWin = (record) => {
+  let result = record.find( record => record.result === "W" )
+  return !!result ? result.year : undefined
+}
