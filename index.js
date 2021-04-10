ac2d41785935e832145ce57e3d3029f1c408b326
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-04-10 13:32:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+  //...
+]
